bc957e1493d78af59d7280cc730bc0a45d17fcdc
Kenny CHU <kennychu@riec.tohoku.ac.jp>
2022-11-05 15:30:36 +0800

    add nvim terminal shortcuts

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -59,6 +59,8 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+keymap("n", "<leader>t", "<cmd>Topen<cr>", opts)
+keymap("n", "<leader>r", "<cmd>Tnew<cr>", opts)
 
 keymap(
 	"n",
--- .config/nvim/lua/user/neoterm.lua
+++ .config/nvim/lua/user/neoterm.lua
@@ -1,1 +1,2 @@
 vim.g.neoterm_default_mod = "belowright"
+vim.g.neoterm_autoinsert = true
