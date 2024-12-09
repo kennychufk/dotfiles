6bde0de326b324f5d2ba0f04102d1a215c6de63f
Kenny CHU <kennychu@riec.tohoku.ac.jp>
2024-12-09 12:16:48 +0800

    show hidden files in telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,11 @@ return {
     local telescope = require("telescope")
     local actions = require("telescope.actions")
     telescope.setup({
+      pickers = {
+        find_files = {
+          hidden = true,
+        }
+      },
       defaults = {
         path_display = { "smart" },
         mappings = {
